60ead87276d0f4a29cc03b0abc0d7bf4ca3e5485
lgh <dlrlgmd25@gmail.com>
2024-04-10 15:51:46 +0900

    react useState(usestate, hooks)

--- src/App.js
+++ src/App.js
@@ -5,16 +5,18 @@ import Home from './Home';
 import Footer from './Footer';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Info from './pages/Info'
+import State from './pages/State'
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/info' element={<Info />} />
-      </Routes>
+        <Header />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/info' element={<Info />} />
+          <Route path='/state' element={<State />} />
+        </Routes>
       </BrowserRouter>
       <Footer />
     </div>
