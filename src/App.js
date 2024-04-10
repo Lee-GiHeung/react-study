1ab1dd669ab74e877a48fd03b95a3babac4e710d
lgh <dlrlgmd25@gmail.com>
2024-04-10 11:32:32 +0900

    react component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 
 import './App.css';
+import Component1 from './Component1';
 
 function App() {
   return (
     <div className="App">
       <h2>hello react</h2>
+      <Component1 />
     </div>
   );
 }
