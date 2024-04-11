c4ea909e8b0f511c664e2b01f4eb800d5eb664a3
lgh <dlrlgmd25@gmail.com>
2024-04-11 14:41:38 +0900

    useSearchParams(query string, useSearchParams, 주소요청)

--- src/App.js
+++ src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Info from './pages/Info';
 import State from './pages/State';
 import Param1 from './pages/Param1';
+import Param2 from './pages/Param2';
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
           <Route path='/info' element={<Info />} />
           <Route path='/state' element={<State />} />
           <Route path='/param/:id' element={<Param1 />} />
+          <Route path='/param' element={<Param2 />} />
         </Routes>
       </BrowserRouter>
       <Footer />
